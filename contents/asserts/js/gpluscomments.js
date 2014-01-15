c7621ad0a91196e776c23004bc2c56adf184191f
Chixq <chilaoqi@gmail.com>
2014-01-15 19:09:22 +0800

    add some files

--- contents/asserts/js/gpluscomments.js
+++ contents/asserts/js/gpluscomments.js
@@ -6,19 +6,27 @@
  * Time: 4:37 PM
  * To change this template use File | Settings | File Templates.
  */
-function openComments(){
-    document.writeln("div id=\'gpluscomments\'></div>");
-    document.writeln("  <script src=\'https://apis.google.com/js/plusone.js\' type=\'text/javascript\'></script>");
-    document.writeln("  <script>");
-    document.writeln("    gapi.comments.render(\'gpluscomments\', {href: \'{{ site.production_url }}{{ page.url }}\', first_party_property: \'BLOGGER\', view_type: \'FILTERED_POSTMOD\'})");
-    document.writeln("  </script>");
-}
+//window.onload = renderCommentsCounts();
+var commentUnfold = false;
 function display(id){
     var target = document.getElementById(id);
     target.style.display=target.style.display=="none"?"block":"none";
 }
 function renderComments(){
-    gapi.comments.render('gpluscomments', {href: window.location.href, first_party_property: 'BLOGGER', view_type: 'FILTERED_POSTMOD'})
+    var commentIcon = document.getElementById('addcomment');
+    commentIcon.className='icon-spin6 animate-spin';
+    if(!commentUnfold) {
+        gapi.comments.render('gpluscomments', {href: window.location.href, first_party_property: 'BLOGGER', view_type: 'FILTERED_POSTMOD'})
+    }
+    commentUnfold = true;
+    setTimeout(recover, 2000);
+    function recover(){
+        commentIcon.className ="icon-comment-alt";
+    }
 }
 
-
+function renderCommentsCounts(){
+    gapi.commentcount.render('commentscounter', {
+        href: window.location
+    });
+}
